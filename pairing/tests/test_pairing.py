4059c602e07d72f0be7f1b6d30e2dbba76de5ba2
mattwthompson <matt.thompson@vanderbilt.edu>
2018-05-31 17:11:06 -0500

    Add test from example given in Sevick1988

--- pairing/tests/test_pairing.py
+++ pairing/tests/test_pairing.py
@@ -3,10 +3,25 @@ Unit and regression test for the pairing package.
 """
 
 # Import package, test suite, and other packages as needed
-import pairing
 import pytest
-import sys
+import numpy as np
 
-def test_pairing_imported():
-    """Sample test, will always pass so long as import statement worked"""
-    assert "pairing" in sys.modules
+import pairing
+
+
+def test_sevick1988():
+    """Test the system desribed in the appendix of Sevick 1988,
+    doi 10.1063/1.454720"""
+    c_D = np.asarray([[1, 0, 0, 0, 1],
+                      [0, 1, 1, 0, 0],
+                      [0, 1, 1, 0, 1],
+                      [0, 0, 0, 1, 0],
+                      [1, 0, 1, 0, 1]])
+
+    c_I = np.asarray([[1, 1, 1, 0, 1],
+                      [1, 1, 1, 0, 1],
+                      [1, 1, 1, 0, 1],
+                      [0, 0, 0, 1, 0],
+                      [1, 1, 1, 0, 1]])
+
+    assert (c_I == pairing.generate_indirect_connectivity(c_D)).all()
